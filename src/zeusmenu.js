134d7c08ca6fac7eb83a5a4f2c4cb9890fb74cd3
GuilhermeGCS-BOT15 <79100010+GuilhermeGCS-BOT15@users.noreply.github.com>
2021-02-15 13:15:50 -0300

    Add files via upload

---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const GCSmenu = (prefix, pushname) => {
+    return `❍ *Comandos do GCS*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.GCSmenu = GCSmenu
